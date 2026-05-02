0e02c7c67e5972794f667bd95c203f3c52919181
iefniar <fejeffrey.uv@gmail.com>
2026-05-02 04:34:59 -0300

    tailwind and daisyui added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DaisyBtn from './components/DaisyBtn.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="bg-primary-content">
+    <DaisyBtn />
+  </div>
 </template>
 
 <style scoped></style>
