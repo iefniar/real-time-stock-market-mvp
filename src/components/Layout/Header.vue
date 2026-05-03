579fbf117f9af4dac93980584c789758f9b1ee9f
iefniar <fejeffrey.uv@gmail.com>
2026-05-03 17:34:30 -0300

    heroicons added

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,1 +1,8 @@
-<template>header</template>
+<script setup lang="ts">
+import { HomeIcon } from '@heroicons/vue/24/outline';
+</script>
+
+<template>
+header
+<HomeIcon class="size-5 text-gray-900" />
+</template>
