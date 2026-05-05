66249804fde18f0409729912adf5b671a6583942
iefniar <fejeffrey.uv@gmail.com>
2026-05-05 04:37:16 -0300

    not found page updated

--- src/components/NotFoundPage.vue
+++ src/components/NotFoundPage.vue
@@ -1,1 +1,17 @@
-<template>Not Found</template>
+<template>
+  Not Found Page
+  <div class="relative min-h-screen overflow-hidden bg-linear-to-b from-[#064e3b] from-0% to-[#000000] to-100%">
+    <!-- Grid Pattern -->
+    <div
+      class="absolute inset-0 h-full w-full opacity-40 bg-black bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-size-[64px_64px]"
+    ></div>
+
+    <!-- Glowing Sphere -->
+    <div
+      class="absolute -top-50 -left-80 h-72 w-2xl rounded-full bg-indigo-500 opacity-30 blur-[100px]"
+    ></div>
+
+    <!-- Content -->
+    <div class="relative z-10 text-gray-200 text-center text-4xl font-bold uppercase">Not Found</div>
+  </div>
+</template>
