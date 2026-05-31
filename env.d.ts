e8473ee2cfa13616de9a9cbc0dab4ad3bb686cfa
iefniar <fejeffrey.uv@gmail.com>
2026-05-31 01:43:41 -0300

    better-auth started

--- env.d.ts
+++ env.d.ts
@@ -5,3 +5,13 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+// Adding TypeScript support for .env variable VITE_BACKEND_URL 
+interface ImportMetaEnv {
+  readonly VITE_BACKEND_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
---
+++ src/lib/auth-client.ts
@@ -0,0 +1,9 @@
+import { createAuthClient } from "better-auth/vue";
+
+export const authClient = createAuthClient({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+
+  fetchOptions: {
+    credentials: "include",
+  },
+});
