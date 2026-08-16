0a0e271af0d1486602371c1894bf3ed199d790b2
iefniar <fejeffrey.uv@gmail.com>
2026-08-16 20:57:24 -0300

    watchlist table updated

--- src/components/Stocks/WatchlistTable.vue
+++ src/components/Stocks/WatchlistTable.vue
@@ -2,12 +2,12 @@
   <div
     class="relative overflow-x-auto p-4 rounded-lg border-neutral-content border bg-secondary-content"
   >
+    <div
+      class="absolute inset-0 opacity-[0.1] dark:opacity-[0.1]"
+      :style="patternStyle"
+    ></div>
     <div class="absolute top-0 left-0 w-1 h-full bg-primary"></div>
-    <table class="table relative">
-      <div
-        class="absolute inset-0 opacity-[0.1] dark:opacity-[0.1] z-0"
-        :style="patternStyle"
-      ></div>
+    <table class="table">
       <thead>
         <tr class="text-primary">
           <th>Company</th>
@@ -20,7 +20,7 @@
           <th></th>
         </tr>
       </thead>
-      <tbody class="z-10">
+      <tbody>
         <tr
           v-for="item in watchlist"
           :key="item.symbol"
